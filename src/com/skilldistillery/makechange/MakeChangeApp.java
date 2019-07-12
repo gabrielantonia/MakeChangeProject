8cb8b85c6e313f2701122fc539cb9e9d410e1461
Gabriel Antonia <Gabriel.Antonia@Outlook.com>
2019-07-12 16:53:17 -0600

    initial commit

---
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.makechange;
+
+import java.util.Scanner;
+
+public class MakeChangeApp {
+
+	public static void main(String[] args) {
+
+		Scanner kb = new Scanner(System.in);
+		System.out.println("");
+		
+	}
+
+}
